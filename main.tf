a942f1f376d56678ab43f8eece8edb19f90f65b3
Aseemshopping <56362954+Aseemshopping@users.noreply.github.com>
2023-07-04 12:34:02 +0530

    Update main.tf
    modified the region & names

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "ap-south-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+resource "aws_instance" "Jenkins-Terraform" {
+  ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "Jenk-Terra-Instance"
   }
 }
